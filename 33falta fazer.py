a6e6b3b02e20e7700ba0efb4bc16f597dcf84e6f
Marinomura <marinomura@hotmail.com>
2023-01-19 11:44:11 -0400

    falta fazer

--- 33falta fazer.py
+++ 33falta fazer.py
@@ -1,12 +1,27 @@
 #Faça um Programa que leia 2 números e em seguida pergunte ao usuário qual operação ele deseja realizar.
 # O resultado da operação deve ser acompanhado de uma frase que diga se o número é:
-
 #par ou ímpar;
 #positivo ou negativo;
 #inteiro ou decimal.
-
 n1 = float(input("Digite o número 1: "))
 n2 = float(input("Digite o número 2: "))
-operacao = input("Digite a operação que deseja realizar: [+, -, /, *]: ")
-par =(n1 or n2 /2)
+op = input("Digite a operação: ")
+
+match(op):
+    case '+':
+        soma =n1+n2
+        print(f'{n1}+{n2} = {soma}')
+
+    case '-':
+        sub = n1-n2
+        print(f'{n1}-{n2}= {sub}')
+
+    case '*':
+        multi = n1*n2
+        print(f'{n1}*{n2}= {multi}')
+
+    case '/' :
+        div = n1/n2
+        print(f'{n1}/{n2} = {div}')
+
 #Se diferente de 0 e divisivel por 2 (' Então o número é par')
---
+++ 67 senha usuario sem repetir.py
@@ -0,0 +1,6 @@
+#Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, 
+# mostrando uma mensagem de erro e voltando a pedir as informações.​
+nome= input('Digite o Nome do usuário ')
+senha= input('Digite a senha do usuário ')
+while nome ==senha:
+    print ("SENHA INVÁLIDA')

965e58b7d97677d1e0b218defa2e7b00c314021e
Marinomura <marinomura@hotmail.com>
2023-01-24 11:37:02 -0400

    24.1.1

--- 40 salario pagar contas.py
+++ 40 salario pagar contas.py
@@ -1,3 +1,11 @@
 #João recebeu seu salário de R$ 1200,00 e precisa pagar duas contas (C1=R$ 200,00 e C2=R$120,00) que estão atrasadas.
 # Como as contas estão atrasadas, João terá de pagar multa de 2% sobre cada conta.
 # Faça um algoritmo que calcule e mostre quanto restará do salário do João
+salario=1200
+c1=200
+c2=120
+juros=0.2
+total=(c1+(c1*juros))+(c1+(c2*juros))
+print (f'O valor total das contas é R$: {total:.2f}')
+resto=salario-total
+print(f'João ficará com: R$ {resto:.2f} de seu salario.')
--- 41 rachar a conta.py
+++ 41 rachar a conta.py
@@ -2,3 +2,12 @@
 # Faça um algoritmo para ler o valor total da conta e imprimir quanto cada um deve pagar, 
 # mas faça com que Joceyr e Thiago não paguem centavos.
 # Ex: uma conta de R$101,53 resulta em R$33,00 para Joceyr, R$33,00 para Thiago e R$35,53 para Alexandre.
+conta=float(input('Digite o valor total da conta a pagar: '))
+pessoas=3
+conta_div=conta % pessoas
+total=(conta / pessoas)
+cont_int=conta//pessoas
+#print ('O valor da conta é R$:',conta)
+print ('Joceyr e Thiago pagarão cada um: R$ ', cont_int)
+pagante_int=cont_int+conta_div
+print (f'Alexandre deverá pagar: R$ {pagante_int:.2f} ')
